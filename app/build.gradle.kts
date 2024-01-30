0d56f216d49a3c6121e38570acb9d4583b09bb45
Ahmed Feki <fkahmed785@gmail.com>
2024-01-30 03:35:15 +0100

    first commit of the app and all modules

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,8 @@ android {
 
 dependencies {
     implementation("androidx.compose.ui:ui:1.6.0")
+    implementation("com.google.android.material:material:1.12.0-alpha03")
+    implementation("androidx.compose.material3:material3:1.2.0-rc01")
     implementation("androidx.compose.material:material:1.6.0")
     implementation("androidx.compose.ui:ui-tooling:1.6.0")
     implementation("androidx.activity:activity-compose:1.9.0-alpha02")
